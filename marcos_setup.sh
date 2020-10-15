88166b5d29a4b02c638743266d609b3b6e28c066
Vlad Negnevitsky <vee neg nev at gee mail dot com>
2020-10-15 03:11:13 +0200

    Clone correct server branch

--- marcos_setup.sh
+++ marcos_setup.sh
@@ -20,7 +20,7 @@ ssh root@$1 "killall marcos_server"
 
 echo "Copying MaRCoS server..."
 # borrowed trick of avoiding .git folder from https://stackoverflow.com/questions/11557114/cp-r-without-hidden-files
-git clone --depth=1 -b rp-125 https://github.com/vnegnev/marcos_server.git /tmp/marcos_server
+git clone --depth=1 -b vnegnev/ocra_grad_ctrl https://github.com/vnegnev/marcos_server.git /tmp/marcos_server
 ssh root@$1 "mkdir /tmp/marcos_server"
 scp -r /tmp/marcos_server/* root@$1:/tmp/marcos_server
 rm -rf /tmp/marcos_server # remove local copy
@@ -32,7 +32,7 @@ mkdir build
 cd build
 cmake ../src
 make -j2
-cp marcos_server ~/
+# cp marcos_server ~/
 # rm -rf /tmp/marcos_server
 EOF
 
