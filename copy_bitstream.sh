2900c8deaa7a443fef2025b290e3a8ae3e82da47
Vlad Negnevitsky <nvlad at phys dot ethz>
2020-10-22 20:27:57 +0200

    Autodetection of standard RP image

--- copy_bitstream.sh
+++ copy_bitstream.sh
@@ -12,12 +12,19 @@ if [[ "$#" -ne 2 || ($2 != "rp-125" && $2 != "rp-122" ) ]]; then
 fi
 
 echo "Copying FPGA bitstream..."
-scp ocra_mri_$2.bit.bin root@$1:/lib/firmware/ocra_mri.bit.bin
-scp ocra_mri.dtbo root@$1:/lib/firmware/
 
-echo "Writing bitstream to FPGA (you should see a blue light appear) ..."
-# borrowed trick from https://stackoverflow.com/questions/305035/how-to-use-ssh-to-run-a-shell-script-on-a-remote-machine
-ssh root@$1 <<EOF
+# Decide whether the target is running the Ocra Linux image, or the
+# standard Red Pitaya one
+rp_uname=$(ssh root@$1 "uname -n")
+if [[ rp_uname -eq "redpitaya" ]]; then
+
+    scp ocra_mri_$2.bit.bin root@$1:/lib/firmware/ocra_mri.bit.bin
+    scp ocra_mri.dtbo root@$1:/lib/firmware/
+
+    echo "Writing bitstream to FPGA, ocra Linux image"
+    echo "(you should see a blue light appear) ..."
+    # borrowed trick from https://stackoverflow.com/questions/305035/how-to-use-ssh-to-run-a-shell-script-on-a-remote-machine
+    ssh root@$1 <<EOF
 #!/bin/bash
 
 if [ -d /sys/kernel/config/device-tree/overlays/full ]; then
@@ -29,3 +36,16 @@ echo 0 > /sys/class/fpga_manager/fpga0/flags
 mkdir /sys/kernel/config/device-tree/overlays/full
 echo -n "ocra_mri.dtbo" > /sys/kernel/config/device-tree/overlays/full/path
 EOF
+
+else
+
+    echo "Writing bitstream to FPGA, standard Red Pitaya Linux image"
+    echo "(you should see a blue light appear) ..."
+    
+    scp ocra_mri_$2.bit root@$1:/tmp/ocra_mri.bit
+    ssh ocra_mri_$2.bit <<EOF
+cat /tmp/ocra_mri.bit > /dev/xdevcfg
+rm /tmp/ocra_mri.bit
+EOF
+
+fi
