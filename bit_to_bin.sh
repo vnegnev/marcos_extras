9eab495dff3b4b1818b9b898f2c8a1873a1edffc
Vlad N <v neg nev at gee mail dot com>
2023-01-08 00:33:59 +0100

    Rename FPGA images

--- bit_to_bin.sh
+++ bit_to_bin.sh
@@ -4,15 +4,15 @@
 #
 # WARNING: doesn't produce updated DTBO files! Only for use with the
 # Vivado GUI workflow, which only generates a bit file.
-# 
+#
 # Need to have bootgen in the PATH
 # Setup on my PC:
 # . /opt/Xilinx/Vivado/2019.2/settings64.sh
 # export PATH=/opt/Xilinx/Scout/2019.1/bin:$PATH
 #
 
-echo "all:{ ocra_mri_rp-122.bit}" > ocra_mri.bif
-bootgen -image ocra_mri.bif -arch zynq -process_bitstream bin -w
-echo "all:{ ocra_mri_rp-125.bit}" > ocra_mri.bif
-bootgen -image ocra_mri.bif -arch zynq -process_bitstream bin -w
-rm ocra_mri.bif
+echo "all:{ marcos_fpga_rp-122.bit}" > marcos_fpga.bif
+bootgen -image marcos_fpga.bif -arch zynq -process_bitstream bin -w
+echo "all:{ marcos_fpga_rp-125.bit}" > marcos_fpga.bif
+bootgen -image marcos_fpga.bif -arch zynq -process_bitstream bin -w
+rm marcos_fpga.bif
